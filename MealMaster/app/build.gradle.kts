7c8222501088f96a87e6e6e407cea2023edce31c
abdelrahman mostafa <103889376+abdelrahman420@users.noreply.github.com>
2024-09-23 20:51:02 +0300

    adding needed dependencies(Retrofit,Room,Lottie,Glide,Json converter)

--- MealMaster/app/build.gradle.kts
+++ MealMaster/app/build.gradle.kts
@@ -40,4 +40,19 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+    // Json Converter
+    implementation("com.google.code.gson:gson:2.11.0")
+    // Retrofit
+    implementation("com.squareup.retrofit2:retrofit:2.11.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.11.0")
+    // Glide
+    implementation("com.github.bumptech.glide:glide:4.16.0")
+    annotationProcessor("com.github.bumptech.glide:compiler:4.16.0")
+    // Room
+    implementation("androidx.room:room-runtime:2.6.1")
+    annotationProcessor("androidx.room:room-compiler:2.6.1")
+
+    //Lotie
+    implementation("com.airbnb.android:lottie:6.5.2")
+
 }
