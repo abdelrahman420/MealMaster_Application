d308a5b9ee110e2eeec5e286a182bf42cfe43a3c
abdelrahman mostafa <103889376+abdelrahman420@users.noreply.github.com>
2024-09-25 14:55:16 +0300

    Creating Splash Activity

--- MealMaster/app/src/main/java/com/example/mealmaster/view/activity/SplashActivity.java
+++ MealMaster/app/src/main/java/com/example/mealmaster/view/activity/SplashActivity.java
@@ -1,6 +1,10 @@
 package com.example.mealmaster.view.activity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,15 +16,23 @@ import com.example.mealmaster.R;
 
 public class SplashActivity extends AppCompatActivity {
 
+    private static final String TAG = "SplashActivity";
+    private TextView txtSplash;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_splash);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        txtSplash= findViewById(R.id.txtSplash);
+        txtSplash.setText("Meal Master");
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },5000);
     }
 }
